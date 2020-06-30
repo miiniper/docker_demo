fbe0c7e708a2319a2bf73e1287e3f305b9053357
miiniper <miiniper@gmail.com>
2020-06-30 16:53:13 +0800

    init build

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -10,13 +11,20 @@ import (
 
 func main() {
 	fmt.Println("start server ... ")
-	ctx := context.Background()
 
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
+	defer cli.Close()
 
+	//ListImages(cli)
+	BuildImage(cli)
+
+}
+
+func ListImages(cli *client.Client) {
+	ctx := context.Background()
 	images, err := cli.ImageList(ctx, types.ImageListOptions{})
 	if err != nil {
 		panic(err)
@@ -27,4 +35,21 @@ func main() {
 		fmt.Println(image.RepoTags)
 	}
 
+}
+
+func BuildImage(cli *client.Client) {
+
+	dockerBuildContext, err := os.Open("/home/han/ifeng/k8scheck/Dockerfile")
+	if err != nil {
+		fmt.Println("11121: ", err)
+	}
+	defer dockerBuildContext.Close()
+
+	_, err = cli.ImageBuild(context.Background(), dockerBuildContext, types.ImageBuildOptions{
+
+		Dockerfile: "Dockerfile",
+	})
+	if err != nil {
+		fmt.Println("11111: ", err)
+	}
 }
